235cc235c1eaae5a7fc2a21f08d65be6349557c9
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-07-01 13:07:08 +0900

    feat : 상품 카테고리 횡 스크롤 위젯 추가
    횡 스크롤로 카테고리 분류 선택

---
+++ lib/widgets/category_horizontal_scroll.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'category_horizontal_scroll_item.dart';
+
+class CategoryHorizontalScroll extends StatefulWidget {
+  const CategoryHorizontalScroll({super.key});
+
+  @override
+  State<StatefulWidget> createState() => CategoryHorizontalScrollState();
+}
+
+class CategoryHorizontalScrollState extends State<CategoryHorizontalScroll> {
+  int selectedIndex = 0;
+  List<String> categories = ["전체", "상의", "하의", "아우터", "신발", "악세사리"];
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(categories.length, (index) {
+          return CategoryHorizontalScrollItem(
+            categories[index],
+            selectedIndex != index,
+            () {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+          );
+        }),
+      ),
+    );
+  }
+}

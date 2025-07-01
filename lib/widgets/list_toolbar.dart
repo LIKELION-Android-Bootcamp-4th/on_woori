abde564d29429b3be34023a3f566df24496cd3ff
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-07-01 13:06:02 +0900

    feat : ListToolbar 위젯 추가
    GridView, ListView 상단에 총 개수 Text 와 편집 버튼 출력

---
+++ lib/widgets/list_toolbar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ListToolbar extends StatelessWidget {
+  int itemCount;
+
+  ListToolbar(this.itemCount);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(24, 0, 24, 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "상품 $itemCount개",
+            style: TextStyle(color: Color(0xFF7D7D7D), fontSize: 16),
+          ),
+          InkWell(
+            child: Text(
+              "편집",
+              style: TextStyle(
+                color: Color(0xFF4A90E2),
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+              ),
+            ),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}

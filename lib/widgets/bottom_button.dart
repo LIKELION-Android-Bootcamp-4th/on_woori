8a2c6401e997d78e3313f9cc03da7633c4486e92
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-07-02 10:29:38 +0900

    feat : Bottom Button 위젯 추가
    여러 화면에서 공통으로 사용되는 Bottom Button 위젯

---
+++ lib/widgets/bottom_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../core/styles/app_colors.dart';
+
+class BottomButton extends StatelessWidget {
+  String buttonText;
+
+  BottomButton(this.buttonText, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 60,
+      margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.primarySub,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16)
+            )
+        ),
+        child: Text(
+          buttonText,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}

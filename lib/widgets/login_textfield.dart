ec4e2cc7a5e431a4f4fbc783ed313ab1dc9216f8
h1jn2 <139345734+h1jn2@users.noreply.github.com>
2025-07-04 11:49:08 +0900

    fix : TextField 유효성 검증
    유효성 검증되도록 TextFormField 로 수정

--- lib/widgets/login_textfield.dart
+++ lib/widgets/login_textfield.dart
@@ -5,42 +5,46 @@ import '../core/styles/app_colors.dart';
 class LoginTextField extends StatelessWidget {
   String labelText;
   String hintText;
+  TextInputType inputType;
+  bool isPassword;
+  TextEditingController? controller;
+  String? Function(String?)? validator;
 
-  LoginTextField({required this.labelText, required this.hintText, super.key});
+  LoginTextField({
+    required this.labelText,
+    required this.hintText,
+    required this.inputType,
+    this.controller,
+    this.validator,
+    this.isPassword = false,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      obscureText: true,
+    return TextFormField(
+      controller: controller,
+      validator: validator,
+      obscureText: isPassword,
       decoration: InputDecoration(
         labelText: labelText,
         hintText: hintText,
         labelStyle: TextStyle(color: AppColors.primarySub),
         focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(10.0),
-          ),
-          borderSide: BorderSide(
-            width: 1,
-            color: Colors.redAccent,
-          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderSide: BorderSide(width: 1, color: Colors.redAccent),
         ),
         enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(10.0),
-          ),
-          borderSide: BorderSide(
-            width: 1,
-            color: Colors.redAccent,
-          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderSide: BorderSide(width: 1, color: Colors.redAccent),
         ),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(10.0),
-          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
+        contentPadding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+        errorStyle: TextStyle(color: AppColors.editDeleteTextButton, fontSize: 12),
       ),
-      keyboardType: TextInputType.visiblePassword,
+      keyboardType: inputType,
     );
   }
 }

fd1348d3b55b4adb1540d1183923e6e912e68bc8
haebun <mediafactory1279@gmail.com>
2025-07-07 14:16:33 +0900

    fix : 로그인 응답값 복원
    develop 에 나중에 반영된 응답값 복원

--- lib/data/entity/response/auth/login_response.g.dart
+++ lib/data/entity/response/auth/login_response.g.dart
@@ -11,9 +11,7 @@ LoginResponse _$LoginResponseFromJson(Map<String, dynamic> json) =>
       success: json['success'] as bool,
       message: json['message'] as String,
       data: LoginData.fromJson(json['data'] as Map<String, dynamic>),
-      timestamp: json['timestamp'] == null
-          ? null
-          : DateTime.parse(json['timestamp'] as String),
+      timestamp: DateTime.parse(json['timestamp'] as String),
     );
 
 Map<String, dynamic> _$LoginResponseToJson(LoginResponse instance) =>
@@ -21,7 +19,7 @@ Map<String, dynamic> _$LoginResponseToJson(LoginResponse instance) =>
       'success': instance.success,
       'message': instance.message,
       'data': instance.data.toJson(),
-      'timestamp': instance.timestamp?.toIso8601String(),
+      'timestamp': instance.timestamp.toIso8601String(),
     };
 
 LoginData _$LoginDataFromJson(Map<String, dynamic> json) => LoginData(
@@ -37,9 +35,7 @@ Map<String, dynamic> _$LoginDataToJson(LoginData instance) => <String, dynamic>{
 };
 
 UserData _$UserDataFromJson(Map<String, dynamic> json) => UserData(
-  id: json['id'] as String,
   email: json['email'] as String,
-  name: json['name'] as String?,
   nickName: json['nickName'] as String?,
   profile: json['profile'] == null
       ? null
@@ -58,16 +54,11 @@ UserData _$UserDataFromJson(Map<String, dynamic> json) => UserData(
   updatedAt: json['updatedAt'] == null
       ? null
       : DateTime.parse(json['updatedAt'] as String),
-);
-
-Map<String, dynamic> _$UserDataToJson(UserData instance) => <String, dynamic>{
-  'id': instance.id,
   v: (json['__v'] as num?)?.toInt(),
 );
 
 Map<String, dynamic> _$UserDataToJson(UserData instance) => <String, dynamic>{
   'email': instance.email,
-  'name': instance.name,
   'nickName': instance.nickName,
   'profile': instance.profile?.toJson(),
   'platformRoles': instance.platformRoles,
